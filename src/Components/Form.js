a944a30d2ed889af1ffdced59c96eef3db192a3d
lukkstar <janjgava.98@icloud.com>
2021-07-23 14:45:58 +0400

    React Todo

---
+++ src/Components/Form.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Form = ({ setInputText, inputText, todos, setTodos, setStatus }) => {
+  const inputTextHandler = e => {
+    setInputText(e.target.value);
+  };
+  const submitTodoHandler = e => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText('');
+  };
+  const filterHandler = e => {
+    setStatus(e.target.value);
+  };
+  return (
+    <form>
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        className="todo-input"
+        type="text"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={filterHandler} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
